a17ac5a96faf548b64fb8fdca0339a39dc2fd47f
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-04-07 20:00:02 -0300

    Incluindo teste para testar ofertas antes do inicio do leilao

--- Alura.LeilaoOnline.Tests/LeilaoRecebeLance.cs
+++ Alura.LeilaoOnline.Tests/LeilaoRecebeLance.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace Alura.LeilaoOnline.Tests
 {
-    public class LeilaoRecebeOferta
+    public class LeilaoRecebeLance
     {
         [Theory]
         [InlineData(2, new double[] { 800, 900 })]
@@ -27,6 +27,26 @@ namespace Alura.LeilaoOnline.Tests
             leilao.RecebeLance(fulano, 1000);
 
             //Assert
+            var qtdObtida = leilao.Lances.Count();
+
+            Assert.Equal(qtdEsperada, qtdObtida);
+        }
+
+        [Theory]
+        [InlineData(0, new double[] { 200, 300, 400, 500 })]
+        [InlineData(0, new double[] { 200 })]
+        [InlineData(0, new double[] { 200, 300, 400 })]
+        [InlineData(0, new double[] { 200, 300, 400, 500, 600, 700 })]
+        public void NaoPermiteLanceDadoQuePregaoNaoFoiIniciado(int qtdEsperada, double[] ofertas)
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano de Tal", leilao);
+
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+
             var qtdObtida = leilao.Lances.Count();
 
             Assert.Equal(qtdEsperada, qtdObtida);
