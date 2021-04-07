3b02161f8959dac270530ffdb66c7c24d4081df7
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-04-06 23:05:09 -0300

    Aperfeiçoando testes divindo as resposabilidades

--- Alura.LeilaoOnline.ConsoleApp/Program.cs
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -5,8 +5,26 @@ namespace Alura.LeilaoOnline.ConsoleApp
 {
     class Program
     {
-        static void Main()
+        private static void Verifica(double esperado, double obtido)
         {
+            var cor = Console.ForegroundColor;
+            if (esperado == obtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(
+                    $"TESTE FALHOU! Esperado: {esperado}, obtido: {obtido}.");
+            }
+            Console.ForegroundColor = cor;
+        }
+
+        private static void LeilaoComApenasUmLance()
+        {
+            //Arrange - cenários
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
             var maria = new Interessada("Maria", leilao);
@@ -16,9 +34,42 @@ namespace Alura.LeilaoOnline.ConsoleApp
             leilao.RecebeLance(fulano, 1000);
             leilao.RecebeLance(fulano, 990);
 
+            //Act - método sob teste
             leilao.TerminaPregao();
 
-            Console.WriteLine(leilao.Ganhador.Valor);
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        private static void LeilaoComVariosLances()
+        {
+            //Arrange - cenários
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(fulano, 990);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        static void Main()
+        {
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
         }
     }
 }
